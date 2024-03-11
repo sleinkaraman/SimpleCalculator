8e13e9d78f962475eb7bdd2526831ede232cb822
sleinkaraman <162487122+sleinkaraman@users.noreply.github.com>
2024-03-11 19:51:17 +0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Selin Karaman on 11.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
