f02c78600717239bbb7a8195e34d0ffddec0ee63
sleinkaraman <162487122+sleinkaraman@users.noreply.github.com>
2024-03-12 12:48:53 +0300

    SimpleCalculator
    Simple calculator application

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var result=0
 
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sumClicked(_ sender: Any) {
+        if let firstNumber=Int(firstText.text!){ // if let -> eğer ki firstText'i int'e çevirebilirsen devam et
+            if let secondNumber=Int(secondText.text!){
+                
+                result=firstNumber+secondNumber
+                resultLabel.text=String(result)
+            }
+        }
+    }
+    
+    
+    
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        if let firstNumber=Int(firstText.text!){
+            if let secondNumber=Int(secondText.text!){
+                
+                result=firstNumber-secondNumber
+                resultLabel.text=String(result)
+            }
+        }
+    }
+    
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        if let firstNumber=Int(firstText.text!){
+            if let secondNumber=Int(secondText.text!){
+                
+               result=firstNumber*secondNumber
+                resultLabel.text=String(result)
+            }
+        }
+    }
+    
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        if let firstNumber=Int(firstText.text!){
+            if let secondNumber=Int(secondText.text!){
+                
+                result=firstNumber/secondNumber
+                resultLabel.text=String(result)
+            }
+        }
+    }
+    
+    
+    
 
 }
 
